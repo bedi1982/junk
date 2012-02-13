3d3f2c97bfa14e49ab75de5520aa4f65a4b461c9
Sergio Rafael Lemke <bedi.com@gmail.com>
2012-02-13 18:21:50 -0200

    playing with c++

--- C++/hello_world.cpp
+++ C++/hello_world.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 void teste(int n1, int n2)
 {
-	cout << "n1 + n2" << endl;
-	cout << "Seu burro, entre aspas vai imprimir o texto." <<endl;
+	int soma;
+	soma = n1 + n2;
+
+	cout << "n1 + n2 = " << soma << ", sacou?" << endl;
+/*	cout << "Seu burro, entre aspas vai imprimir o texto." <<endl;*/
 }
 
 int main()
