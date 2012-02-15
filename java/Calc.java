bdb26443f70f1257ceaad70a5102bbdaf6c8ebab
bedi1982 <bedi.com@gmail.com>
2012-02-15 13:09:11 -0200

    Adding some java

---
+++ java/Calc.java
@@ -0,0 +1,22 @@
+public class Calc {
+
+       public static void main(String[] args) {
+    	   System.out.println(soma(2,2));
+    	   }
+       
+       public static int soma(int a, int b) {
+    	   return a + b;
+       }
+
+       public static int subtrai(int a, int b) {
+    	   return a - b;
+       }
+
+       public static int multiplica(int a, int b) {
+    	   return a * b;
+       }
+
+       public static int divide(int a, int b) {
+    	   return a / b; 
+       }
+}
