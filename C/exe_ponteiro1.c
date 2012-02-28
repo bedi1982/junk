30d686201601be281e6491c3ca3dc49ab03c9c4d
Sergio Rafael Lemke <bedi.com@gmail.com>
2012-02-28 17:52:20 -0300

    adding a pointers exercise

---
+++ C/exe_ponteiro1.c
@@ -0,0 +1,37 @@
+#include <time.h>
+#include <stdio.h>
+
+// Elabore um programa que armazene valores aleatórios em um vetor de inteiros de 10 posições e depois, em outro vetor de ponteiros de inteiros de tamanho 10, coloque os endereços dos valores do vetor de inteiros de forma ordenada crescente, ficando a primeira posição do vetor de ponteiros com o endereço do menor valor até a última posição que conterá o endereço do maior valor.
+
+int main()
+{
+	int inteiros[9];
+	int *enderecos[9];
+	int i,j;
+	int *tmp;
+
+	srand ( time(NULL) );
+	
+	for(i=0; i<10; i++){
+		inteiros[i] = rand()%100;
+	}
+	
+	for(i=0; i<10; i++){
+		enderecos[i] = &inteiros[i];
+	}
+/*	
+	for(i=0; i<10; i++){
+		for(j=0; j<10; j++){
+			if(*enderecos[j] < *enderecos[j+1]){
+				tmp = enderecos[j];
+				enderecos[i] = tmp;
+				enderecos[j] = enderecos[i];
+			}
+		}
+	}
+*/
+	for(i=0; i<10; i++){
+		printf ("%p\n", enderecos[i]);
+	}
+
+}
