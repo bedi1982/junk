444a7bbb27c6722cebb6244e333e3f4484f9ac1c
Sergio Rafael Lemke <bedi.com@gmail.com>
2014-08-26 14:14:15 -0300

    Update importator.py

--- importator.py
+++ importator.py
@@ -1,22 +1,38 @@
 import os, sys
-op = raw_input('Insert 1 to Import. Insert 2 to Export.\n')
+###########SOME FUNCTIONS###################################
+def export_from_dev():
+        rtc = raw_input('Insert RTC number\n')
 
+##Create the premium folder in tmp if it not exists:
+        if not os.path.exists('/tmp/premium/'):
+                print '/tmp/premium folder not exists, -not a problem, creating and proceeding...\n'
+                os.makedirs('/tmp/premium/')
+###################################################
+
+        path = '/tmp/premium/'+ str(rtc)
+        print 'Your CTE package and related logs will be created at:\n', path,'\n'
+        os.mkdir(path, 0755)
+
+#############################################################
+
+op = raw_input('Insert 1 to Import. Insert 2 to Export.\n')
 if op.isdigit():
         if(op == '1'):
                 print 'Import selected.\n'
         elif(op == '2'):
                 print 'Export selected.\n'
+                export_from_dev()
+
         elif((op < 0) or (op > 2)):
                 print 'Invalid Option, exiting.\n'
+                sys.exit(0)
 else:
-        print 'Invalid Option, exiting.\n'
+        print 'Invalid Option, exiting. Only 1 or 2 are valid options, letters not.\n'
+        sys.exit(0)
 
-path = '/tmp/sergio'
-os.mkdir(path, )
-
-def export_from_dev():
-        print 'Insert RTC number'
-        print 'Generate in /tmp'
+#path = '/tmp/sergio/'+ str(rtc)
+#print path
+#os.mkdir(path, 0755)
 
 def import_into_sit():
         print 'Insert RTC number'
