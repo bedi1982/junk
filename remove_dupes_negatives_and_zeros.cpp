df64544808e5f91a21a5c9c0fae317336ebb0985
Sergio Rafael Lemke <srlemke@protonmail.com>
2018-10-19 16:53:54 +0200

    added exercice resolution

---
+++ remove_dupes_negatives_and_zeros.cpp
@@ -0,0 +1,39 @@
+//#include <stdio.h>
+#include <vector> //vector
+#include <iostream> //cout
+#include <algorithm> //sort
+
+int main(){
+	
+	//my vector
+	std::vector<int> A = {-1,0,0,0,2,3,6,4,1,2,0,0,0,0,-1,-2,-3};
+
+	//remove negatives and 0s
+	for (int i=0; i<A.size(); i++ ){
+		if(A[i] == 0 || A[i] < 1){
+			A.erase(A.begin()+i);
+			i--;
+		}
+	}
+
+	//sort and remove dupes 
+	//https://stackoverflow.com/questions/1041620/whats-the-most-efficient-way-to-erase-duplicates-and-sort-a-vector#1041939 //
+	sort(A.begin(), A.end());
+	A.erase(unique(A.begin(), A.end()), A.end());
+	
+	//just a print to see array status
+	std::cout << "Vector status after cleanup: \n";
+	for (int i=0; i<A.size(); i++ ){
+		std::cout << " "  << A[i];
+	}
+
+	//compare iterator to value (iterator)
+	for (int i=1; i<=A.size(); i++ ){
+		if(i != A[i-1]){
+			std::cout << "\n\nSmallest mising: " << i << "\n";
+			break;
+		}
+	}
+	return 0;
+}
+
